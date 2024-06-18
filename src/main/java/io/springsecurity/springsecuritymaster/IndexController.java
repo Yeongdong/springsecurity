52bc71860a4f638ba5b443f9f0d367ec518e507d
Yeongdong <jyd37855@gmail.com>
2024-06-18 13:53:06 +0900

    formLogin 학습

---
+++ src/main/java/io/springsecurity/springsecuritymaster/IndexController.java
@@ -0,0 +1,22 @@
+package io.springsecurity.springsecuritymaster;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+}
