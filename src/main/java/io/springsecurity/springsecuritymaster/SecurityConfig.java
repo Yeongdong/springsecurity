6eaf22f68bb5ee614a9d4269367983ae88888963
Yeongdong <jyd37855@gmail.com>
2024-06-18 19:11:58 +0900

    RequestCache/SavedRequest 학습

--- src/main/java/io/springsecurity/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/springsecurity/springsecuritymaster/SecurityConfig.java
@@ -1,18 +1,16 @@
 package io.springsecurity.springsecuritymaster;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 @EnableWebSecurity
 @Configuration
@@ -20,27 +18,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName("customParam=y");
+
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/logoutSuccess").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout
-                        .logoutUrl("/logoutProc")
-                        .logoutRequestMatcher(new AntPathRequestMatcher("logoutProc", "POST"))
-                        .logoutSuccessUrl("/logoutSuccess")
-                        .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/logoutSuccess"))
-                        .deleteCookies("JSESSIONID", "remember-me")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .addLogoutHandler((request, response, authentication) -> {
-                            HttpSession session = request.getSession();
-                            session.invalidate();
-                            SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
-                            SecurityContextHolder.getContextHolderStrategy().clearContext();
+                .formLogin(form -> form
+                        .successHandler((request, response, authentication) -> {
+                            SavedRequest savedRequest = requestCache.getRequest(request, response);
+                            String redirectUrl = savedRequest.getRedirectUrl();
+                            response.sendRedirect(redirectUrl);
                         })
-                        .permitAll()
-                );
+                )
+                .requestCache(cache -> cache.requestCache(requestCache))
+        ;
         return http.build();
     }
 
