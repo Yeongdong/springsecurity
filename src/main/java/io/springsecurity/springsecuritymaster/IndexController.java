32505edba0b931e0844ccae4a07b658ccc6477aa
Yeongdong <jyd37855@gmail.com>
2024-06-21 14:25:04 +0900

    cors 학습

--- src/main/java/io/springsecurity/springsecuritymaster/IndexController.java
+++ src/main/java/io/springsecurity/springsecuritymaster/IndexController.java
@@ -12,17 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class IndexController {
 
-    private final SessionInfoService sessionInfoService;
-
     @GetMapping("/")
     public Authentication index(Authentication authentication) {
         return authentication;
-    }
-
-    @GetMapping("/sessionInfo")
-    public String sessionInfo() {
-        sessionInfoService.sessionInfo();
-        return "sessionInfo";
     }
 
     @GetMapping("/home")
@@ -67,5 +59,20 @@ public class IndexController {
     @GetMapping("/expiredUrl")
     public String expiredUrl() {
         return "expiredUrl";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/denied")
+    public String denied() {
+        return "denied";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 }
