97829d7f7ac60e50ee9dab7f95a32d0ab1b8db01
Yeongdong <jyd37855@gmail.com>
2024-06-22 19:18:56 +0900

    커스텀 권한부여(WebExpressionAuthorizationManager) 학습

--- src/main/java/io/springsecurity/springsecuritymaster/IndexController.java
+++ src/main/java/io/springsecurity/springsecuritymaster/IndexController.java
@@ -1,58 +1,29 @@
 package io.springsecurity.springsecuritymaster;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.CookieGenerator;
 
 @RestController
 public class IndexController {
 
-    @GetMapping("/user")
-    public String user(){
-        return "user";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
-    @GetMapping("/myPage/points")
-    public String myPage(){
-        return "myPage";
+    @GetMapping("/custom")
+    public String custom(){
+        return "custom";
     }
 
-    @GetMapping("/manager")
-    public String manager(){
-        return "manager";
+    @GetMapping("/user/{name}")
+    public String userName(@PathVariable(value = "name") String name){
+        return name;
     }
 
-    @GetMapping("/admin")
+    @GetMapping("/admin/db")
     public String admin(){
         return "admin";
-    }
-
-    @GetMapping("/admin/payment")
-    public String adminPayment(){
-        return "adminPayment";
-    }
-
-    @GetMapping("/resource/address_01")
-    public String address_01(){
-        return "address_01";
-    }
-
-    @GetMapping("/resource/address01")
-    public String address01(){
-        return "address01";
-    }
-
-    @PostMapping("/post")
-    public String post(){
-        return "post";
     }
 }
