5cddabc4fafd7618999fa2aba0d808264eb5e815
Yeongdong <jyd37855@gmail.com>
2024-06-22 17:28:30 +0900

    sameSite 학습

--- src/main/java/io/springsecurity/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/springsecurity/springsecuritymaster/SecurityConfig.java
@@ -10,31 +10,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        SpaCsrfTokenRequestHandler csrfTokenRequestHandler = new SpaCsrfTokenRequestHandler();
-
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/csrf", "/csrfToken", "/cookie", "/cookieCsrf").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .csrfTokenRequestHandler(csrfTokenRequestHandler)
-                )
-                .addFilterBefore(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
-        return http.build();
-    }
-
-    /*@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
@@ -45,7 +26,7 @@ public class SecurityConfig {
                 .csrf(Customizer.withDefaults())
         ;
         return http.build();
-    }*/
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
