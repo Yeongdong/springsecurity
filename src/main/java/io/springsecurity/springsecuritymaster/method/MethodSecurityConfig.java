5339de303c1a8077cf1c0c6c1bd8f1f1211416e9
Yeongdong <jyd37855@gmail.com>
2024-06-24 14:28:20 +0900

    AOP 메서드보안 학습

--- src/main/java/io/springsecurity/springsecuritymaster/method/MethodSecurityConfig.java
+++ src/main/java/io/springsecurity/springsecuritymaster/method/MethodSecurityConfig.java
@@ -1,12 +1,16 @@
 package io.springsecurity.springsecuritymaster.method;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
-import org.springframework.security.authorization.method.AuthorizationManagerAfterMethodInterceptor;
-import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableMethodSecurity(prePostEnabled = false)
@@ -14,14 +18,22 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 public class MethodSecurityConfig {
 
     @Bean
-    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    public Advisor preAuthorize() {
-        return AuthorizationManagerBeforeMethodInterceptor.preAuthorize(new MyPreAuthorizationManager());
+    public MethodInterceptor methodInterceptor() {
+        AuthenticatedAuthorizationManager<MethodInvocation> authorizationManager = new AuthenticatedAuthorizationManager<>();
+        return new CustomMethodInterceptor(authorizationManager);
     }
 
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    public Advisor postAuthorize() {
-        return AuthorizationManagerAfterMethodInterceptor.postAuthorize(new MyPostAuthorizationManager());
+    public Pointcut pointcut() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* io.security.springsecuritymaster.DataService.*(..))");
+        return pointcut;
     }
+
+    @Bean
+    public Advisor serviceAdvisor() {
+        return new DefaultPointcutAdvisor(pointcut(), methodInterceptor());
+    }
+
 }
