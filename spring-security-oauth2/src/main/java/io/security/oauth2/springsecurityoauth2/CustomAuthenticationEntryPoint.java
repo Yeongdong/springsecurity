17486b3584d2bb9a994e24013bfacc2e4837e050
Yeongdong <jyd37855@gmail.com>
2024-06-30 14:29:48 +0900

    AuthenticationEntryPoint 학습

---
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,16 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        System.out.println("제일 우선적으로 실행");
+    }
+}
--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -4,30 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.
                 authorizeHttpRequests(authorized -> authorized
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults());
-//                .with(new CustomSecurityConfigurer().setFlag(true), Customizer.withDefaults());
-        return http.build();
-    }
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+        ;
 
-    @Bean
-    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests(authorized -> authorized
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
-//                .with(new CustomSecurityConfigurer().setFlag(true), Customizer.withDefaults());
         return http.build();
     }
 }
