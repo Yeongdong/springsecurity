8ef1a4eb4cc282d632874ad72592a978930b75a1
Yeongdong <jyd37855@gmail.com>
2024-07-02 17:29:36 +0900

    springMvc 인증 객체 참조 학습

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -1,5 +1,10 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,24 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public OAuth2User user(Authentication authentication) {
+//        OAuth2AuthenticationToken authentication1 = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationToken authentication2 = (OAuth2AuthenticationToken) authentication;
+
+        OAuth2User oAuth2User = authentication2.getPrincipal();
+        return oAuth2User;
+    }
+
+    @GetMapping("/oauth2User")
+    public OAuth2User oauth2User(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        return oAuth2User;
+    }
+
+    @GetMapping("/oidcUser")
+    public OAuth2User oidcUser(@AuthenticationPrincipal OidcUser oAuth2User) {
+        return oAuth2User;
     }
 }
