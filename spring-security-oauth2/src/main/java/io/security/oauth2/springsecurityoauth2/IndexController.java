11b2db02f417cb725ce4f16e82fe8cf8c0acd1db
Yeongdong <jyd37855@gmail.com>
2024-07-02 19:27:01 +0900

    oauth2AuthorizationRequestResolver 학습

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -1,10 +1,5 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,29 +1,37 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfig {
 
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/home").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(login -> login
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login/v1/oauth2/code/*")
-                        .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig
-                                .baseUri("/oauth2/v1/authorization"))
-                        .redirectionEndpoint(redirectionEndpointConfig -> redirectionEndpointConfig
-                                .baseUri("/login/v1/oauth2/code/*"))
+                        .authorizationEndpoint(authEndpoint -> authEndpoint
+                                .authorizationRequestResolver(customOAuth2AuthenticationRequestResolver()))
                 )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/home"))
         ;
         return http.build();
+    }
+
+    private OAuth2AuthorizationRequestResolver customOAuth2AuthenticationRequestResolver() {
+        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
 }
