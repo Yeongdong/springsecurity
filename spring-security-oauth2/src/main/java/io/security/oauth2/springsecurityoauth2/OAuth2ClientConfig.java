5f9561d6163738d49dfcd24e5221cae08daf9b2b
Yeongdong <jyd37855@gmail.com>
2024-07-02 17:12:49 +0900

    logout 학습

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,19 +1,43 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
+@Configuration
 public class OAuth2ClientConfig {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
-                .oauth2Login(Customizer.withDefaults());
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                )
+        ;
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+
+        return successHandler;
     }
 }
