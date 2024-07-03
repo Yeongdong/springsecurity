2dc343449e098e49df80646281fcdeeed72aaa64
Yeongdong <jyd37855@gmail.com>
2024-07-03 12:23:13 +0900

    OAuth2AuthorizedClient 학습

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,37 +1,25 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfig {
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/home").permitAll()
+                        .requestMatchers("/home", "/client").permitAll()
                         .anyRequest().authenticated())
-                .oauth2Login(login -> login
-                        .authorizationEndpoint(authEndpoint -> authEndpoint
-                                .authorizationRequestResolver(customOAuth2AuthenticationRequestResolver()))
-                )
+                .oauth2Client(Customizer.withDefaults())
                 .logout(logout -> logout
-                        .logoutSuccessUrl("/home"))
+                        .logoutSuccessUrl("/logout"))
         ;
         return http.build();
-    }
-
-    private OAuth2AuthorizationRequestResolver customOAuth2AuthenticationRequestResolver() {
-        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
 }
