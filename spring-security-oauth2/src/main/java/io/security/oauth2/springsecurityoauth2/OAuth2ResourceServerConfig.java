2c39f07f402ac64ad995b2c3eef905f741d2dc45
Yeongdong <jyd37855@gmail.com>
2024-07-08 19:41:45 +0900

    OpaqueToken 검사 학습

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServerConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServerConfig.java
@@ -1,12 +1,12 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,31 +15,23 @@ public class OAuth2ResourceServerConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomRoleConverter());
-
         http.
                 authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.GET, "/photos/1").hasAuthority("ROLE_photo")
-                        .requestMatchers(HttpMethod.GET, "/photos/3").hasAuthority("ROLE_default-roles-oauth2")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwtAuthenticationConverter(jwtAuthenticationConverter)
-                        ))
+                        .opaqueToken(Customizer.withDefaults()))
         ;
         return http.build();
     }
 
+    /*@Bean
+    public OpaqueTokenIntrospector opaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
+        OAuth2ResourceServerProperties.Opaquetoken opaquetoken = properties.getOpaquetoken();
+        return new NimbusOpaqueTokenIntrospector(opaquetoken.getIntrospectionUri(), opaquetoken.getClientId(), opaquetoken.getClientSecret());
+    }*/
+
     @Bean
-    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.GET, "/photos/2").permitAll()
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oAuth2ResourceServerConfig -> oAuth2ResourceServerConfig.jwt(Customizer.withDefaults()))
-        ;
-        return http.build();
+    public OpaqueTokenIntrospector opaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
+        return new CustomOpaqueTokenIntrospector(properties);
     }
 }
