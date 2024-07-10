19647bb089552d4526f51e83ac621b0f82dc53c8
Yeongdong <jyd37855@gmail.com>
2024-07-10 15:09:43 +0900

    AuthorizationServer 주요 도메인 학습

---
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/RegisteredClientController.java
@@ -0,0 +1,24 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RegisteredClientController {
+    @Autowired
+    private RegisteredClientRepository registeredClientRepository;
+
+    @GetMapping("/registeredClients")
+    public List<RegisteredClient> getRegisteredClients() {
+        RegisteredClient registeredClient1 = registeredClientRepository.findByClientId("oauth2-client-app1");
+        RegisteredClient registeredClient2 = registeredClientRepository.findByClientId("oauth2-client-app2");
+        RegisteredClient registeredClient3 = registeredClientRepository.findByClientId("oauth2-client-app3");
+
+        return List.of(registeredClient1, registeredClient2, registeredClient3);
+    }
+}
